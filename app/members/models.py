0c2f459e2a6e48088480c87a71b32230b8d634f8
parkhongbeen <pack122@naver.com>
2020-05-20 22:18:55 +0900

    login기능구현

--- app/members/models.py
+++ app/members/models.py
@@ -3,5 +3,9 @@ from django.db import models
 
 
 class User(AbstractUser):
-    # username = models.CharField('username', max_length=20)
-    name = models.CharField('이름', max_length=20)
+    username = models.CharField('이름', max_length=50, unique=True)
+    password = models.CharField('비밀번호', max_length=50)
+    useremail = models.EmailField('이메일', max_length=50)
+
+    def __str__(self):
+        return self.username
--- app/members/views.py
+++ app/members/views.py
@@ -1,6 +1,13 @@
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from members.forms import LoginForm
 
 
 def login_view(request):
-    return render(request, 'members/login.html')
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, 'members/login.html', {'form': form})
