10b9675a5cc224ec62dfd0810d731ecf7b0cbf12
parkhongbeen <pack122@naver.com>
2020-05-22 15:01:46 +0900

    소셜로그인 facebookuser 정보 가져오기 성공

--- app/members/urls.py
+++ app/members/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('naver-login/', views.naver_login, name='naver-login'),
+    path('facebook-login/', views.facebook_login, name='facebook-login'),
 
 ]
--- app/members/views.py
+++ app/members/views.py
@@ -9,33 +9,6 @@ from members.forms import LoginForm, SignupForm
 from members.models import User
 
 
-def login_view(request):
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            request.session['user'] = form.user_id
-            return redirect('/')
-    else:
-        form = LoginForm()
-
-    login_base_url = 'https://nid.naver.com/oauth2.0/authorize'
-    login_params = {
-        'response_type': 'code',
-        'client_id': '_P0gJW9IsjsuCscC6V5Y	',
-        'redirect_uri': 'http://localhost:8000/members/naver-login/',
-        'state': 'RANDOM_STATE',
-    }
-    login_url = '{base}?{params}'.format(
-        base=login_base_url,
-        params='&'.join([f'{key}={value}' for key, value in login_params.items()])
-    )
-    context = {
-        'form': form,
-        'login_url': login_url,
-    }
-    return render(request, 'members/login.html', context=context)
-
-
 def logout_view(request):
     logout(request)
     return redirect('members:login')
@@ -51,6 +24,37 @@ def signup_view(request):
     else:
         form = SignupForm()
     return render(request, 'members/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
+            return redirect('/')
+    else:
+        form = LoginForm()
+
+    # 네이버 로그인
+    n_login_base_url = 'https://nid.naver.com/oauth2.0/authorize'
+    n_login_params = {
+        'response_type': 'code',
+        'client_id': '_P0gJW9IsjsuCscC6V5Y',
+        'redirect_uri': 'http://localhost:8000/members/naver-login/',
+        'state': 'RANDOM_STATE',
+    }
+    n_login_url = '{base}?{params}'.format(
+        base=n_login_base_url,
+        params='&'.join([f'{key}={value}' for key, value in n_login_params.items()])
+    )
+
+    # 페이스북 로그인
+    context = {
+        'form': form,
+        'app_id': app_id,
+        'n_login_url': n_login_url,
+    }
+    return render(request, 'members/login.html', context=context)
 
 
 def naver_login(request):
@@ -90,3 +94,43 @@ def naver_login(request):
         user = User.objects.get(username=naver_username)
     login(request, user)
     return render(request, 'home/home.html')
+
+
+app_id = 602161200404788
+app_secret = '8f0614ff67b38e2908983c5ef913dd4c'
+
+
+def facebook_login(request):
+    code = request.GET['code']
+    redirect_uri = f'http://localhost:8000/members/facebook-login'
+    url_access_token = "https://graph.facebook.com/v2.11/oauth/access_token"
+    params_access_toekn = {
+        "client_id": app_id,
+        "redirect_uri": redirect_uri,
+        "client_secret": app_secret,
+        "code": code,
+    }
+
+    response = requests.get(url_access_token, params=params_access_toekn)
+    # url_debug_token = 'https://graph.facebook.com/debug_token'
+    # params_debug_token = {
+    #     "input_token": response.json()['access_token'],
+    #     "access_token": f'{app_id}|{app_secret}'
+    # }
+    # user_info = requests.get(url_debug_token, params=params_debug_token)
+
+    url_user_info = 'https://graph.facebook.com/me'
+    user_info_fields = [
+        'id',
+        'first_name',
+        'last_name',
+        'picture',
+    ]
+    access_token = response.json()['access_token']
+    params_user_info = {
+        "fields": ','.join(user_info_fields),
+        "access_token": access_token
+    }
+    user_info = requests.get(url_user_info, params=params_user_info)
+
+    return HttpResponse(user_info.json().items())
